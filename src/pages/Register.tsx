e2e5703c0fa388c5d128cc4eccb2d27ec1bede10
Maik <estevan.maik@gmail.com>
2023-09-06 17:07:14 -0300

    feat: created Register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,25 @@
+export const Register = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <h4>Sign in</h4>
+          <form>
+            <div>
+              <input />
+              <input />
+            </div>
+
+            <div>
+              <>
+                <button>Register</button>
+                <button>I already have an account</button>
+              </>
+            </div>
+          </form>
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
