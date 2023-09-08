8f014806f4802b59f06c7a4a95bfca6a1410e941
Maik <estevan.maik@gmail.com>
2023-09-08 20:47:44 -0300

    feat: created app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import './App.css';
-import { Register } from './pages/Register';
+import Register from 'pages/Register';
+import Login from 'pages/Login';
+import Home from 'pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <>
-      <Register />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<div>NOT FOUND</div>} />
+      </Routes>
+    </Router>
   );
 }
 
