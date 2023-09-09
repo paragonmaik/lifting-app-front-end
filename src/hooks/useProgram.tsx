decf30a85f06e160d6956269467aa42a6904cf0f
Maik <estevan.maik@gmail.com>
2023-09-08 21:15:13 -0300

    feat: created useProgram custom hook

---
+++ src/hooks/useProgram.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import { axiosRequest } from 'utils/axiosRequest';
+import { Program } from 'types';
+import { useLocalStorage } from './useLocalStorage';
+
+export default function useProgram() {
+  const [token, _setToken] = useLocalStorage('token', '');
+  return useQuery({
+    queryKey: ['programs'],
+    retry: 0,
+    queryFn: async (): Promise<Program[]> => {
+      const res = await axiosRequest({
+        method: 'get',
+        url: '/api/programs',
+        headers: {
+          Authorization: token,
+        },
+      });
+
+      return res.data as Program[];
+    },
+  });
+}
