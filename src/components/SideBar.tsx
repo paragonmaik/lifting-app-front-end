ec66e8d0c3f8b5598d8ef43d84704f3066d75117
Maik <estevan.maik@gmail.com>
2023-09-10 14:39:12 -0300

    feat: added placeholder collapse button to the sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,19 +1,26 @@
 import useProgram from 'hooks/useProgram';
+import AddProgramForm from './AddProgramModal';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Program } from 'types';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Context } from 'context/Context';
 
 export default function SideBar() {
+  const [show, setShow] = useState(false);
   const { data: programs } = useProgram();
   const { setCurProgramPos } = useContext(Context);
 
   return (
     <div className="h-100">
-      <Sidebar className="h-100" collapsed={false}>
+      <Sidebar className="h-100" collapsed={show}>
         <Menu>
+          <MenuItem onClick={() => setShow(!show)}>
+            <div className="d-flex">
+              Logo placeholder<div>Collapse Placeholder</div>
+            </div>
+          </MenuItem>
+          <AddProgramForm />
           <SubMenu label="Programs">
-            <MenuItem>Add Program</MenuItem>
             {programs?.map((program: Program, i) => (
               <MenuItem onClick={() => setCurProgramPos(i)} key={program.id}>
                 {program.name}
