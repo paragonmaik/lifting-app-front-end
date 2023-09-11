ce39e3514fa6196ac8b34b5a52c669c1c2e01f5c
Maik <estevan.maik@gmail.com>
2023-09-11 20:33:20 -0300

    feat: added handleEditExercise method to the EditExerciseModal

--- src/components/EditExerciseModal.tsx
+++ src/components/EditExerciseModal.tsx
@@ -1,8 +1,11 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import BaseModal from './ui/BaseModal';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { Goal } from 'types';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosRequest } from 'utils/axiosRequest';
 
 type ExerciseDTO = {
   id?: number;
@@ -21,7 +24,44 @@ export default function EditExerciseModal({
   goal,
   restSeconds,
 }: ExerciseDTO) {
+  const [token, _setToken] = useLocalStorage('token', '');
   const [show, setShow] = useState(false);
+  const queryClient = useQueryClient();
+
+  const { mutate } = useMutation({
+    mutationFn: axiosRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['programs']);
+      setShow(!show);
+    },
+  });
+
+  function handleEditExercise(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const { exerciseName, instructions, load, goal, restSeconds } =
+      e.target as typeof e.currentTarget;
+
+    const data = {
+      id,
+      goal: goal.value,
+      name: exerciseName.value,
+      instructinos: instructions.value,
+      load: load.value,
+      restSeconds: restSeconds.value,
+    };
+
+    console.log(data);
+
+    mutate({
+      method: 'put',
+      url: '/api/exercises',
+      headers: {
+        Authorization: token,
+      },
+      data,
+    });
+  }
 
   return (
     <>
@@ -33,8 +73,8 @@ export default function EditExerciseModal({
         show={show}
         setShow={setShow}
         children={
-          <Form>
-            <Form.Group className="mb-3" controlId="">
+          <Form onSubmit={handleEditExercise}>
+            <Form.Group className="mb-3" controlId="exerciseName">
               <Form.Label>Exercise Name</Form.Label>
               <Form.Control
                 defaultValue={name}
@@ -42,7 +82,7 @@ export default function EditExerciseModal({
                 placeholder="Enter name"
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="instructions">
               <Form.Label>Exercise instructions</Form.Label>
               <Form.Control
                 defaultValue={instructions}
@@ -50,7 +90,7 @@ export default function EditExerciseModal({
                 placeholder="Enter instructions"
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="load">
               <Form.Label>Exercise Load</Form.Label>
               <Form.Control
                 defaultValue={load}
@@ -58,7 +98,7 @@ export default function EditExerciseModal({
                 placeholder="Enter load (in kg)"
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="goal">
               <Form.Label>Exercise Goal</Form.Label>
               <Form.Control
                 defaultValue={goal}
@@ -66,7 +106,7 @@ export default function EditExerciseModal({
                 placeholder="Enter goal"
               />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="restSeconds">
               <Form.Label>Exercise Rest Period</Form.Label>
               <Form.Control
                 defaultValue={restSeconds}
