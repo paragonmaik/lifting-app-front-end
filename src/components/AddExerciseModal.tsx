3a32bc5e5140979fb748a6d16efe3f3669190187
Maik <estevan.maik@gmail.com>
2023-09-11 13:49:14 -0300

    feat: handleAddExercise method

--- src/components/AddExerciseModal.tsx
+++ src/components/AddExerciseModal.tsx
@@ -1,11 +1,49 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import BaseModal from './ui/BaseModal';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { axiosRequest } from 'utils/axiosRequest';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export default function AddExerciseModal() {
+  const queryClient = useQueryClient();
   const [show, setShow] = useState(false);
 
+  const createExerciseMutation = useMutation({
+    mutationFn: axiosRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['programs']);
+
+      setShow(!show);
+    },
+  });
+
+  function handleAddExercise(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const { exerciseName, instructions, load, goal, restPeriod } =
+      e.target as typeof e.currentTarget;
+
+    goal.value = 'STRENGTH';
+    exerciseName.value;
+    instructions.value;
+    load.value;
+    restPeriod.value;
+
+    createExerciseMutation.mutate({
+      method: 'post',
+      url: '/api/exercises/1',
+      headers: {
+        Authorization: '',
+      },
+      data: {
+        name: exerciseName.value,
+        instructions: instructions.value,
+        load: load.value,
+        goal: goal.value,
+        restPeriod: restPeriod.value,
+      },
+    });
+  }
   return (
     <>
       <Button variant="primary" onClick={() => setShow(!show)}>
@@ -16,25 +54,25 @@ export default function AddExerciseModal() {
         show={show}
         setShow={setShow}
         children={
-          <Form>
-            <Form.Group className="mb-3" controlId="">
+          <Form onSubmit={handleAddExercise}>
+            <Form.Group className="mb-3" controlId="exerciseName">
               <Form.Label>Exercise Name</Form.Label>
               <Form.Control type="text" placeholder="Enter name" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="instructions">
               <Form.Label>Exercise Instructions</Form.Label>
               <Form.Control type="text" placeholder="Enter instructions" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="load">
               <Form.Label>Exercise Load</Form.Label>
-              <Form.Control type="text" placeholder="Enter load (in kg)" />
+              <Form.Control type="number" placeholder="Enter load (in kg)" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="goal">
               {/* mudar tipo de input */}
               <Form.Label>Exercise Goal</Form.Label>
               <Form.Control type="text" placeholder="Enter goal" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="restPeriod">
               <Form.Label>Exercise Rest Period</Form.Label>
               <Form.Control
                 type="number"
