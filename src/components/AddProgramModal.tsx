b7cc13d5be0dfbb7d48d2d91020c2530c5de0561
Maik <estevan.maik@gmail.com>
2023-09-12 09:09:41 -0300

    feat: added handleAddProgram method to the AddProgramModal component

--- src/components/AddProgramModal.tsx
+++ src/components/AddProgramModal.tsx
@@ -1,12 +1,46 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import BaseModal from './ui/BaseModal';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { MenuItem } from 'react-pro-sidebar';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { axiosRequest } from 'utils/axiosRequest';
 
 export default function AddProgramModal() {
+  const [token, _setToken] = useLocalStorage('token', '');
   const [show, setShow] = useState(false);
+  const queryClient = useQueryClient();
 
+  const { mutate } = useMutation({
+    mutationFn: axiosRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['programs']);
+      setShow(!show);
+    },
+  });
+
+  function handleAddProgram(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const { programName, description, durationWeeks } =
+      e.target as typeof e.currentTarget;
+
+    const data = {
+      name: programName.value,
+      description: description.value,
+      durationMins: durationWeeks.value,
+    };
+
+    mutate({
+      method: 'post',
+      url: '/api/programs',
+      headers: {
+        Authorization: token,
+      },
+      data,
+    });
+  }
   return (
     <>
       <MenuItem onClick={() => setShow(!show)}>Add Program</MenuItem>
@@ -15,16 +49,16 @@ export default function AddProgramModal() {
         show={show}
         setShow={setShow}
         children={
-          <Form>
-            <Form.Group className="mb-3" controlId="">
+          <Form onSubmit={handleAddProgram}>
+            <Form.Group className="mb-3" controlId="programName">
               <Form.Label>Program Name</Form.Label>
               <Form.Control type="text" placeholder="Enter name" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="description">
               <Form.Label>Program description</Form.Label>
               <Form.Control type="text" placeholder="Enter description" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="">
+            <Form.Group className="mb-3" controlId="durationWeeks">
               <Form.Label>Program duration</Form.Label>
               <Form.Control
                 type="number"
