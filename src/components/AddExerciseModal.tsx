bc6b59dc4625b397d49fd2ce3aca86a7d4c8b075
Maik <estevan.maik@gmail.com>
2023-09-12 12:21:12 -0300

    feat: AddExerciseModal now calls the BaseExerciseForm component

--- src/components/AddExerciseModal.tsx
+++ src/components/AddExerciseModal.tsx
@@ -1,6 +1,6 @@
-import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import BaseModal from './ui/BaseModal';
+import BaseExerciseForm from './ui/BaseExerciseForm';
 import { FormEvent, useState } from 'react';
 import { axiosRequest } from 'utils/axiosRequest';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -53,45 +53,11 @@ export default function AddExerciseModal({ workoutId }: { workoutId: number }) {
         show={show}
         setShow={setShow}
         children={
-          <Form onSubmit={handleAddExercise}>
-            <Form.Group className="mb-3" controlId="exerciseName">
-              <Form.Label>Exercise Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="instructions">
-              <Form.Label>Exercise Instructions</Form.Label>
-              <Form.Control type="text" placeholder="Enter instructions" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="load">
-              <Form.Label>Exercise Load</Form.Label>
-              <Form.Control type="number" placeholder="Enter load (in kg)" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="goal">
-              {/* mudar tipo de input */}
-              <Form.Label>Exercise Goal</Form.Label>
-              <Form.Control type="text" placeholder="Enter goal" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="restSeconds">
-              <Form.Label>Exercise Rest Period</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter rest period (in seconds)"
-              />
-            </Form.Group>
-            <br />
-            <div className="d-flex justify-content-end">
-              <Button
-                onClick={() => setShow(!show)}
-                className="mx-1"
-                variant="secondary"
-              >
-                Cancel
-              </Button>
-              <Button className="mx-1" variant="primary" type="submit">
-                Confirm
-              </Button>
-            </div>
-          </Form>
+          <BaseExerciseForm
+            handleExercise={handleAddExercise}
+            setShow={setShow}
+            show={show}
+          />
         }
       />
     </>
