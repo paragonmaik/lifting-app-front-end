72195e7cca0dd86443690b1c0a8f2e385d189e0a
Maik <estevan.maik@gmail.com>
2023-09-12 12:11:26 -0300

    feat: AddWorkoutModal now calls the BaseWorkoutForm component

--- src/components/AddWorkoutModal.tsx
+++ src/components/AddWorkoutModal.tsx
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import BaseModal from './ui/BaseModal';
 import { FormEvent, useState } from 'react';
 import { useLocalStorage } from 'hooks/useLocalStorage';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosRequest } from 'utils/axiosRequest';
+import BaseWorkoutForm from './ui/BaseWorkoutForm';
+import Button from 'react-bootstrap/Button';
+import BaseModal from './ui/BaseModal';
 
 export default function AddWorkoutModal({ programId }: { programId: number }) {
   const [token, _setToken] = useLocalStorage('token', '');
@@ -51,36 +51,11 @@ export default function AddWorkoutModal({ programId }: { programId: number }) {
         show={show}
         setShow={setShow}
         children={
-          <Form onSubmit={handleAddWorkout}>
-            <Form.Group className="mb-3" controlId="workoutName">
-              <Form.Label>Workout Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="description">
-              <Form.Label>Workout description</Form.Label>
-              <Form.Control type="text" placeholder="Enter description" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="durationMins">
-              <Form.Label>Workout duration</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter duration (in minutes)"
-              />
-            </Form.Group>
-            <br />
-            <div className="d-flex justify-content-end">
-              <Button
-                onClick={() => setShow(!show)}
-                className="mx-1"
-                variant="secondary"
-              >
-                Cancel
-              </Button>
-              <Button className="mx-1" variant="primary" type="submit">
-                Confirm
-              </Button>
-            </div>
-          </Form>
+          <BaseWorkoutForm
+            handleWorkout={handleAddWorkout}
+            setShow={setShow}
+            show={show}
+          />
         }
       />
     </>
