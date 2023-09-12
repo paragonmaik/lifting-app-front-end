335be61abe08a35c987ffc7430ebdaeeb6ae29d4
Maik <estevan.maik@gmail.com>
2023-09-12 10:48:49 -0300

    refactor: AddProgramModal now uses BaseProgramForm component

--- src/components/AddProgramModal.tsx
+++ src/components/AddProgramModal.tsx
@@ -1,11 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import BaseModal from './ui/BaseModal';
 import { FormEvent, useState } from 'react';
 import { MenuItem } from 'react-pro-sidebar';
 import { useLocalStorage } from 'hooks/useLocalStorage';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { axiosRequest } from 'utils/axiosRequest';
+import BaseModal from './ui/BaseModal';
+import BaseProgramForm from './ui/BaseProgramForm';
 
 export default function AddProgramModal() {
   const [token, _setToken] = useLocalStorage('token', '');
@@ -50,36 +49,11 @@ export default function AddProgramModal() {
         show={show}
         setShow={setShow}
         children={
-          <Form onSubmit={handleAddProgram}>
-            <Form.Group className="mb-3" controlId="programName">
-              <Form.Label>Program Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="description">
-              <Form.Label>Program description</Form.Label>
-              <Form.Control type="text" placeholder="Enter description" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="durationWeeks">
-              <Form.Label>Program duration</Form.Label>
-              <Form.Control
-                type="number"
-                placeholder="Enter duration (in weeks)"
-              />
-            </Form.Group>
-            <br />
-            <div className="d-flex justify-content-end">
-              <Button
-                onClick={() => setShow(!show)}
-                className="mx-1"
-                variant="secondary"
-              >
-                Cancel
-              </Button>
-              <Button className="mx-1" variant="primary" type="submit">
-                Confirm
-              </Button>
-            </div>
-          </Form>
+          <BaseProgramForm
+            handleProgram={handleAddProgram}
+            setShow={setShow}
+            show={show}
+          />
         }
       />
     </>
