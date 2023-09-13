4938239212362df7e8ab55f21dacf4bb2bd90ab0
Maik <estevan.maik@gmail.com>
2023-09-13 18:23:48 -0300

    feat: added delete program button

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -2,6 +2,7 @@ import WorkoutCard from './WorkoutCard';
 import EditProgramModal from './EditProgramModal';
 import AddWorkoutModal from './AddWorkoutModal';
 import { Program } from 'types';
+import { Button } from 'react-bootstrap';
 
 export default function ProgramCard(program: Program) {
   return (
@@ -10,7 +11,12 @@ export default function ProgramCard(program: Program) {
         <div className="p-2 mt-1 mb-1">
           <div className="my-1 d-flex justify-content-around align-items-center">
             <h1>{program.name}</h1>
-            <EditProgramModal {...program} />
+            <div>
+              <Button className="mx-1" variant="danger">
+                Delete Program
+              </Button>
+              <EditProgramModal {...program} />
+            </div>
           </div>
           <div className="my-1 w-100 d-flex justify-content-around align-items-center">
             <span>Duration: {program.durationWeeks} weeks</span>
