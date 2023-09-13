8527a1810b809544f18599f78b7f75ef6be4b78f
Maik <estevan.maik@gmail.com>
2023-09-13 09:07:18 -0300

    fix: fixed bug where error was null

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,22 +1,32 @@
 import ProgramCard from './ProgramCard';
 import { AxiosError } from 'axios';
 import { ErrorObject, Program } from 'types';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Context } from 'context/Context';
 
 type MainComponentProps = {
+  isError: boolean;
   error: AxiosError;
   programs?: Program[];
 };
 
-export default function MainComponent({ error, programs }: MainComponentProps) {
+export default function MainComponent({
+  error,
+  programs,
+  isError,
+}: MainComponentProps) {
   const { curProgramPos } = useContext(Context);
-  const errorResponse = error.response?.data as ErrorObject;
+  const [errorData, setErrorData] = useState<ErrorObject>();
+
+  if (isError) {
+    const err = error.response?.data as ErrorObject;
+    setErrorData(err);
+  }
 
   return (
     <>
       {!programs ? (
-        <h1>{errorResponse.message}</h1>
+        <h1>{errorData?.message}</h1>
       ) : (
         <ProgramCard {...programs[curProgramPos]} />
       )}
