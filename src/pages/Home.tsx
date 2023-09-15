bc809203e3cb26d8598b26239bf20f58c182229c
Maik <estevan.maik@gmail.com>
2023-09-15 17:12:06 -0300

    refactor: refactored conditional rendering logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import MainComponent from 'components/MainComponent';
 import { AxiosError } from 'axios';
 
 export default function Home() {
-  const { data: programs, isLoading, error } = useProgram();
+  const { data: programs, isLoading, error, isError } = useProgram();
 
   return (
     <main className="d-flex h-100">
@@ -13,7 +13,11 @@ export default function Home() {
         {isLoading ? (
           <h1>Loading...</h1>
         ) : (
-          <MainComponent error={error as AxiosError} programs={programs} />
+          <MainComponent
+            isError={isError}
+            error={error as AxiosError}
+            programs={programs}
+          />
         )}
       </section>
     </main>
