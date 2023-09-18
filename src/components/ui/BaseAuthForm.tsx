53b59418e7233bbcd48c112de635f16198f3af14
Maik <estevan.maik@gmail.com>
2023-09-18 15:34:46 -0300

    feat: added floatinLabes to the BaseAuthForm component

--- src/components/ui/BaseAuthForm.tsx
+++ src/components/ui/BaseAuthForm.tsx
@@ -1,7 +1,9 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import { FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 type AuthType = 'register' | 'login';
 
@@ -9,47 +11,72 @@ type BaseAuthFormProps = {
   authType: AuthType;
   navigateUrl: string;
   handleSubmit?: (e: FormEvent<HTMLFormElement>) => void;
+  errorMessage: string | undefined;
 };
 
 export default function BaseAuthForm({
   authType,
   navigateUrl,
   handleSubmit,
+  errorMessage,
 }: BaseAuthFormProps) {
   const navigate = useNavigate();
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="email">
-        <Form.Label>E-mail</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="password1">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" />
-      </Form.Group>
-      {authType === 'register' ? (
-        <Form.Group className="mb-3" controlId="password2">
-          <Form.Label>Re-enter Password</Form.Label>
-          <Form.Control type="password" />
-        </Form.Group>
-      ) : null}
-      <br />
-      <div className="d-flex justify-content-end">
-        <Button className="mx-1" variant="primary" type="submit">
-          {authType === 'register' ? 'Register' : 'Login'}
-        </Button>
-        <Button
-          onClick={() => navigate(navigateUrl)}
-          className="mx-1"
-          variant="secondary"
-          type="button"
-        >
-          {authType === 'register'
-            ? 'I already have an account'
-            : 'Register a new account'}
-        </Button>
+    <div className="d-flex flex-column justify-content-center h-75">
+      <h1>{authType === 'register' ? 'Create Account' : 'Sign in'}</h1>
+      <div className="d-flex justify-content-center">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="email">
+            <FloatingLabel controlId="email" label="Email" className="mb-3">
+              <Form.Control type="email" placeholder="Email" />
+            </FloatingLabel>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="password1">
+            <FloatingLabel
+              controlId="password1"
+              label="Password"
+              className="mb-3"
+            >
+              <Form.Control type="password" placeholder="Password" />
+            </FloatingLabel>
+          </Form.Group>
+          {authType === 'register' ? (
+            <Form.Group className="mb-3" controlId="password2">
+              <FloatingLabel
+                controlId="password2"
+                label="Re-enter Password"
+                className="mb-3"
+              >
+                <Form.Control type="password" placeholder="Re-enter Password" />
+              </FloatingLabel>
+            </Form.Group>
+          ) : null}
+          <br />
+          <div className="d-flex justify-content-end">
+            <Button className="mx-1" variant="primary" type="submit">
+              {authType === 'register' ? 'Register' : 'Login'}
+            </Button>
+            <Button
+              onClick={() => navigate(navigateUrl)}
+              className="mx-1"
+              variant="secondary"
+              type="button"
+            >
+              {authType === 'register'
+                ? 'I already have an account'
+                : 'Register a new account'}
+            </Button>
+          </div>
+          <div>
+            {errorMessage ? (
+              <Alert className="my-3" variant="danger">
+                {errorMessage}
+              </Alert>
+            ) : null}
+          </div>
+        </Form>
       </div>
-    </Form>
+    </div>
   );
 }
