e75ad134009e1974e93cd6c28daa1380d1c68e61
Maik <estevan.maik@gmail.com>
2023-09-18 13:14:04 -0300

    refactor: refactored request logic to use tanstack query

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,36 +1,55 @@
 import { Dispatch, FormEvent, SetStateAction, useState } from 'react';
 import { axiosRequest } from 'utils/axiosRequest';
-import { NavigateFunction, useNavigate } from 'react-router';
-import { AxiosError, HttpStatusCode } from 'axios';
+import { useNavigate } from 'react-router';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 
-const validatePassword = (
-  password1: string,
-  password2: string
-): string | void => {
-  if (password1.length < 8) {
-    return 'Password must be at least 8 characters long';
-  }
-  if (password1 !== password2) {
-    return 'Passwords must match!';
-  }
-};
+export default function Register() {
+  const [errorMessage, setErrorMessage] = useState<string>();
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
-async function handleRegister(
-  e: FormEvent<HTMLFormElement>,
-  setErrorMessage: Dispatch<SetStateAction<string | undefined>>,
-  navigate: NavigateFunction
-) {
-  e.preventDefault();
-  const { email, password1, password2 } = e.target as typeof e.currentTarget;
-  const validatedPassword = validatePassword(password1.value, password2.value);
+  const validatePassword = (
+    password1: string,
+    password2: string
+  ): string | void => {
+    if (password1.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+    if (password1 !== password2) {
+      return 'Passwords must match!';
+    }
+  };
 
-  if (validatedPassword) {
-    setErrorMessage(validatedPassword);
-    return;
-  }
+  const { mutate } = useMutation({
+    mutationFn: axiosRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['programs']);
+      navigate('/login');
+    },
+    onError: (error: AxiosError) => {
+      const data: any = error?.response?.data;
+      setErrorMessage(data.message);
+    },
+  });
 
-  try {
-    const { status } = await axiosRequest({
+  async function handleRegister(
+    e: FormEvent<HTMLFormElement>,
+    setErrorMessage: Dispatch<SetStateAction<string | undefined>>
+  ) {
+    e.preventDefault();
+    const { email, password1, password2 } = e.target as typeof e.currentTarget;
+    const validatedPassword = validatePassword(
+      password1.value,
+      password2.value
+    );
+
+    if (validatedPassword) {
+      setErrorMessage(validatedPassword);
+      return;
+    }
+
+    mutate({
       method: 'post',
       url: '/api/auth/register',
       data: {
@@ -39,57 +58,37 @@ async function handleRegister(
         role: 0,
       },
     });
-
-    if (status === HttpStatusCode.Ok) {
-      navigate('/login');
-    }
-  } catch (error) {
-    const res = error as AxiosError;
-    const data: any = res.response?.data;
-    if (!data) return;
-
-    setErrorMessage(data.message);
   }
-}
-
-export default function Register() {
-  const [errorMessage, setErrorMessage] = useState<string>();
-  const navigate = useNavigate();
 
   return (
-    <>
+    <div>
       <div>
-        <div>
-          <h4>Register</h4>
-          <span>{errorMessage}</span>
-          <form onSubmit={(e) => handleRegister(e, setErrorMessage, navigate)}>
-            <div>
-              <label>
-                E-mail
-                <input required type="email" id="email" />
-              </label>
-              <label>
-                Password
-                <input required type="password" id="password1" />
-              </label>
-              <label>
-                Re-enter Password
-                <input required type="password" id="password2" />
-              </label>
-            </div>
-
-            <div>
-              <>
-                <button type="submit">Register</button>
-                <button type="button" onClick={() => navigate('/login')}>
-                  I already have an account
-                </button>
-              </>
-            </div>
-          </form>
-          <div></div>
-        </div>
+        <h4>Register</h4>
+        <span>{errorMessage}</span>
+        <form onSubmit={(e) => handleRegister(e, setErrorMessage)}>
+          <div>
+            <label>
+              E-mail
+              <input required type="email" id="email" />
+            </label>
+            <label>
+              Password
+              <input required type="password" id="password1" />
+            </label>
+            <label>
+              Re-enter Password
+              <input required type="password" id="password2" />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Register</button>
+            <button type="button" onClick={() => navigate('/login')}>
+              I already have an account
+            </button>
+          </div>
+        </form>
+        <div></div>
       </div>
-    </>
+    </div>
   );
 }
