45ec89220c6f545c496fa22c2c2e897e3f01bc3a
Maik <estevan.maik@gmail.com>
2023-09-22 13:58:59 -0300

    feat: moved ExerciseDTO to the types file

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,17 @@ export type ErrorObject = {
   message: string;
 };
 
+export type ExerciseDTO = {
+  id?: number;
+  name: string;
+  instructions: string;
+  load: number;
+  goal: Goal;
+  restSeconds: number;
+  sets: number;
+  reps: number;
+};
+
 export type Exercise = {
   id: number;
   userId?: string;
