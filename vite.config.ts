d5d6ba5d9207ffb21a3bfe8c5ae80033a82d4b05
maik <estevan.maik@gmail.com>
2023-09-29 15:55:24 -0300

    feat: updated config file

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  preview: {
+    host: true,
+    port: 3000,
+  },
 });
