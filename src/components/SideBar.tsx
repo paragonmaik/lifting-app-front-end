67f74e0f133c54e9bf9c37d37b0dc180b8f899f6
maik <estevan.maik@gmail.com>
2023-10-12 16:36:08 -0300

    feat: sidebar now auto collapses on low width screens

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,20 +1,32 @@
 import useProgram from 'hooks/useProgram';
 import ProgramModal from './ProgramModal';
+import useScreenSize from 'hooks/useScreenSize';
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import { Program } from 'types';
 import { useContext, useState } from 'react';
 import { Context } from 'context/Context';
 
 export default function SideBar() {
-  const [show, setShow] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
   const { data: programs } = useProgram();
   const { setCurProgramPos } = useContext(Context);
+  const [screenSize, _setScreenSize] = useScreenSize();
+
+  function collapseSidebar(): void {
+    if (screenSize.width < 800) {
+      return;
+    }
+    setCollapsed(!collapsed);
+  }
 
   return (
     <div className="h-100">
-      <Sidebar className="h-100" collapsed={show}>
+      <Sidebar
+        className="h-100"
+        collapsed={screenSize.width < 800 ? true : collapsed}
+      >
         <Menu>
-          <MenuItem onClick={() => setShow(!show)}>
+          <MenuItem onClick={() => collapseSidebar()}>
             <div className="d-flex">
               Logo placeholder<div>Collapse Placeholder</div>
             </div>
