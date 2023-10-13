332ebc71f4e46245111014d587ff84efdad3c810
maik <estevan.maik@gmail.com>
2023-10-13 18:00:35 -0300

    chore: added responsivenes to the BaseExerciseForm

--- src/components/ui/BaseExerciseForm.tsx
+++ src/components/ui/BaseExerciseForm.tsx
@@ -18,61 +18,63 @@ export default function BaseExerciseForm({
 }: ExerciseFormType) {
   return (
     <Form onSubmit={handleExercise}>
-      <Form.Group className="mb-3" controlId="exerciseName">
-        <Form.Label>Exercise Name</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.name}
-          type="text"
-          placeholder="Enter name"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="instructions">
-        <Form.Label>Exercise instructions</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.instructions}
-          type="text"
-          placeholder="Enter instructions"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="sets">
-        <Form.Label>Exercise Sets</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.sets}
-          type="number"
-          placeholder="Enter how many sets"
-        />
-      </Form.Group>
-      <Form.Group className="mb-2" controlId="reps">
-        <Form.Label>Exercise Reps</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.reps}
-          type="number"
-          placeholder="Enter how many reps"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="load">
-        <Form.Label>Exercise Load</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.load}
-          type="number"
-          placeholder="Enter load (in kg)"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="goal">
-        <Form.Label>Exercise Goal</Form.Label>
-        <Form.Select defaultValue={exerciseDTO?.goal} aria-label="">
-          <option value="STRENGTH">Strength</option>
-          <option value="HYPERTROPHY">Hypertrophy</option>
-        </Form.Select>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="restSeconds">
-        <Form.Label>Exercise Rest Period</Form.Label>
-        <Form.Control
-          defaultValue={exerciseDTO?.restSeconds}
-          type="number"
-          placeholder="Enter rest period (in seconds)"
-        />
-      </Form.Group>
+      <div style={{ height: '65vh' }} className="overflow-auto">
+        <Form.Group className="mb-3" controlId="exerciseName">
+          <Form.Label>Exercise Name</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.name}
+            type="text"
+            placeholder="Enter name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="instructions">
+          <Form.Label>Exercise instructions</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.instructions}
+            type="text"
+            placeholder="Enter instructions"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sets">
+          <Form.Label>Exercise Sets</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.sets}
+            type="number"
+            placeholder="Enter how many sets"
+          />
+        </Form.Group>
+        <Form.Group className="mb-2" controlId="reps">
+          <Form.Label>Exercise Reps</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.reps}
+            type="number"
+            placeholder="Enter how many reps"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="load">
+          <Form.Label>Exercise Load</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.load}
+            type="number"
+            placeholder="Enter load (in kg)"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="goal">
+          <Form.Label>Exercise Goal</Form.Label>
+          <Form.Select defaultValue={exerciseDTO?.goal} aria-label="">
+            <option value="STRENGTH">Strength</option>
+            <option value="HYPERTROPHY">Hypertrophy</option>
+          </Form.Select>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="restSeconds">
+          <Form.Label>Exercise Rest Period</Form.Label>
+          <Form.Control
+            defaultValue={exerciseDTO?.restSeconds}
+            type="number"
+            placeholder="Enter rest period (in seconds)"
+          />
+        </Form.Group>
+      </div>
       <br />
       <div className="d-flex justify-content-end">
         <Button
